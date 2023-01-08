3be7a27e0472e74ba9d12cec852b61bdd918b4a0
Jake Weinstein <jake@aospa.co>
2023-01-08 07:55:33 +0000

    display: Make reserved_size in gralloc handle optional
    Few devices that use older stock blobs don't support this yet.
    Allow them to disable it via setting
    TARGET_GRALLOC_HANDLE_HAS_NO_RESERVED_SIZE := true
    in BoardConfig, otherwise enabled by default.
    
    Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ie99b4365ec5f0d0f2c11af9b749046886a11b47d

--- config/display-product-commonsys.mk
+++ config/display-product-commonsys.mk
@@ -14,8 +14,13 @@ SOONG_CONFIG_qtidisplaycommonsys := displayconfig_enabled
 SOONG_CONFIG_qtiunifeddraw := qtiunifeddraw_enabled
 # Soong Values
 SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := false
+SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_reserved_size := false
 SOONG_CONFIG_qtiunifeddraw_qtiunifeddraw_enabled := true
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := true
+endif
+
+ifeq ($(TARGET_GRALLOC_HANDLE_HAS_NO_RESERVED_SIZE),true)
+    SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_reserved_size := true
 endif
